ef0c835aa63fb13a02be7c14312da8344cbabdd9
minhle <leminh16041996@gmail.com>
2022-10-12 09:43:09 +0700

    add menu mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,14 @@
+var btnMenu = document.querySelector("#btn-menu");
+var menuMobile = document.querySelector("#menu--mobile-js");
+console.log(menuMobile);
 
+btnMenu.onclick = function () {
+  menuMobile.classList.add("show");
+  console.log("ok");
+};
+var btnClose = document.querySelector("#btn-close");
+console.log(btnClose);
+btnClose.onclick = function () {
+  menuMobile.classList.remove("show");
+  console.log("hidden");
+};
